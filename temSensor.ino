22c975db27bf19ce59056544baf7c955e8a40bc2
SUN <sunrise.in.my.dream@gmail.com>
2025-12-11 23:13:36 +0700

    display temp & humidity on OLED screen

--- temSensor.ino
+++ temSensor.ino
@@ -1,14 +1,27 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 }
 
 void loop() {
@@ -20,5 +33,20 @@ void loop() {
   Serial.print("%, Temperature:");
   Serial.print(temperature);
   Serial.println("C");
+
+  display.clearDisplay();
+  display.setTextSize(1.7);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.print("Humidity:");
+  display.print(humidity, 1);
+  display.println("%");
+  display.print("Temperature:");
+  display.print(temperature, 1);
+  display.print("C");
+  display.write(3);
+  display.display();
+  delay(2000);
+  // display.startscrollleft(0x00, 0x07);
 }
 
